5e21f8f5fb8856aabec681f0ce81a38e34300ea7
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-12 19:08:17 +0545

    final optimization

--- contact.php
+++ contact.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" href="contact.css">
         <link rel="stylesheet" href="main.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   
     </head>
     <body>
     <?php include_once "header.php"; ?>
